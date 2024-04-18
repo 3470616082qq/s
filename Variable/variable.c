432b2b426644f51aa2da68e0c7e417a4aa7b3511
asdalin <3470616082@qq.com>
2024-04-18 23:14:24 +0800

    更新

---
+++ Variable/variable.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    //类型   变量名=x
+    int age=10;  //整数变量
+    double weiht=11.2;
+    //打印规则:%d\n=int  %f\n=float %lf\n=double
+    printf("%d\n",age);
+    printf("%lf\n",weiht);
+    return 0;
+}
